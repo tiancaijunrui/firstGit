cb76aece27c077226468838e89e811d3d932c509
zhacongjie <zhacongjie@qgutech>
2016-06-29 00:13:40 +0800

    hello world!

---
+++ src/main/java/helloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * User:ZhaCongJie@HF
+ * Date:2016/6/29/029.
+ * Time:0:12
+ */
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}

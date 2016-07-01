f148e194167e9b67a4e250c738b957472627fbbf
zhacongjie <zhacongjie@qgutech>
2016-07-01 23:20:15 +0800

    2016/7/1

---
+++ jetty/src/main/java/test/test1/test1Action.java
@@ -0,0 +1,12 @@
+package test.test1;
+
+/**
+ * User:ZhaCongJie@HF
+ * Date:2016/7/1/001.
+ * Time:23:05
+ */
+public class test1Action {
+    public String test1() {
+        return "test1";
+    }
+}
